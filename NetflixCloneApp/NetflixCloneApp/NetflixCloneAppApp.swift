6a90158b6d175c9d39f886991acc8ae18e9e1b49
mudrhs1997 <mudrhs1997@daum.net>
2022-08-29 22:04:03 +0900

    Start Netflix Clone App

---
+++ NetflixCloneApp/NetflixCloneApp/NetflixCloneAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixCloneAppApp.swift
+//  NetflixCloneApp
+//
+//  Created by 정명곤 on 2022/08/29.
+//
+
+import SwiftUI
+
+@main
+struct NetflixCloneAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
